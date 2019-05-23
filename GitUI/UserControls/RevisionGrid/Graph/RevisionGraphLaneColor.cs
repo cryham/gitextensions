303ef0d8602bbd25f11c294f91cfb36eb2813ef0
cryham <cryham@gmail.com>
2019-05-23 22:44:59 +0200

    graph lane new colors

--- GitUI/UserControls/RevisionGrid/Graph/RevisionGraphLaneColor.cs
+++ GitUI/UserControls/RevisionGrid/Graph/RevisionGraphLaneColor.cs
@@ -9,17 +9,20 @@ namespace GitUI.UserControls.RevisionGrid.Graph
     {
         internal static readonly IReadOnlyList<Color> PresetGraphColors = new[]
 {
-            Color.FromArgb(240, 36, 117),
-            Color.FromArgb(120, 180, 255), // light blue
-            Color.FromArgb(46, 204, 113),
-            Color.FromArgb(142, 68, 173),
-            Color.FromArgb(231, 76, 60),
-            Color.FromArgb(40, 40, 40),
-            Color.FromArgb(26, 188, 156),
-            Color.FromArgb(241, 196, 15)
+            Color.FromArgb(33, 226, 33),
+            Color.FromArgb(110, 160, 210), // light blue
+            Color.FromArgb(214, 214, 23),
+            Color.FromArgb(32, 203, 203),
+            Color.FromArgb(201, 106, 30),
+            Color.FromArgb(140, 120, 220),
+            Color.FromArgb(26, 128, 216),
+            Color.FromArgb(102, 203, 33),
+            Color.FromArgb(201, 66, 35),
+            Color.FromArgb(202, 33, 160),
+            Color.FromArgb(32, 213, 160),
         };
 
-        public static Color NonRelativeColor { get; } = Color.LightGray;
+        public static Color NonRelativeColor { get; } = Color.Gray;
 
         internal static Brush NonRelativeBrush { get; private set; }
 
