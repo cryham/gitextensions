cfac01d9a996c7c7a3c9292c167bb4f6777f9582
cryham <cryham@gmail.com>
2019-05-27 23:22:19 +0200

    expand settings tree

--- GitUI/CommandsDialogs/SettingsDialog/SettingsTreeViewUserControl.cs
+++ GitUI/CommandsDialogs/SettingsDialog/SettingsTreeViewUserControl.cs
@@ -69,6 +69,7 @@ namespace GitUI.CommandsDialogs.SettingsDialog
             }
 
             node.Tag = page;
+            node.Expand();
             _pages2NodeMap.Add(page.PageReference, node);
             _settingsPages.Add(page);
         }
